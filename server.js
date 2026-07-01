0b1ccaafc33f86d0928a5da82ecb73ed7607b823
shivam kumar <shivamkumar110378@gmail.com>
2026-07-01 19:37:05 +0530

    Deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
